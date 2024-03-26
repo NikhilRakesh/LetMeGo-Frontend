707dcbb592e26aae5a3f1de849e4b237c9ce11fd
nikhil rakesh <nikhilappu112@gmail.com>
2024-03-26 18:04:05 +0530

    second commit

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,10 +1,24 @@
 import { createStore, combineReducers } from 'redux';
 import authReducer from '../Reducers/authReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 const rootReducer = combineReducers({
-    auth: authReducer,
+    auth: persistedAuthReducer,
 });
 
-const store = createStore(rootReducer);
+const store = configureStore({
+    reducer: rootReducer
+});
 
+export const persistor = persistStore(store);
 export default store;
