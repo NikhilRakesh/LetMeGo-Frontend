4fc9ed2cf1ccc48a4894c5f40eb27a8e64b41ca0
nikhil rakesh <nikhilappu112@gmail.com>
2024-03-29 16:33:59 +0530

    seventh commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        "name": "LetMeGo",
+        "short_name": "LMG",
+        "start_url": ".",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#ffffff",
+        "icons": [
+          {
+            "src": "lmg -144.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "lmg 512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
+    })
+  ],
   server: {
-    port: 6001
-  }
-})    
+    port: 6001,
+  },
+});
